eac115435c0430c630beafc04fc4265ab93b6b57
AnimeshRy <ani16september@gmail.com>
2020-12-17 17:07:41 +0530

    app.js commit, module remaining

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const tasksContainer = document.querySelector('[data-tasks]')
 const taskTemplate = document.getElementById('task-template');
 const newTaskForm = document.querySelector('[data-new-task-form]')
 const newTaskInput = document.querySelector('[data-new-task-input]')
-
+const clearCompleteTaskButton = document.querySelector('[data-clear-complete-tasks-button]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
@@ -31,6 +31,12 @@ tasksContainer.addEventListener('click', e => {
         save()
         renderTaskCount(selectedList)
     }
+})
+
+clearCompleteTaskButton.addEventListener('click', e => {
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete)
+    saveAndRender();
 })
 
 deleteListButton.addEventListener('click', e => {
